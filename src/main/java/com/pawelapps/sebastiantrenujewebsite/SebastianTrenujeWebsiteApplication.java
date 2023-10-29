a1505107a528393653f9d591b36748e2f467c410
pawelstankiewicz <pawelsta95@gmail.com>
2023-10-29 20:30:10 +0100

    initial commit

---
+++ src/main/java/com/pawelapps/sebastiantrenujewebsite/SebastianTrenujeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.pawelapps.sebastiantrenujewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebastianTrenujeWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SebastianTrenujeWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pawelapps/sebastiantrenujewebsite/SebastianTrenujeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawelapps.sebastiantrenujewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebastianTrenujeWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
